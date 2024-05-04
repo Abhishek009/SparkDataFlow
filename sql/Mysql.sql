d32fa333858bbe997c732332dd7609fd3479e731
abhishek009 <abhisheksingh2009iu@gmail.com>
2024-05-05 01:50:35 +0800

    Code for Mysql

--- sql/Mysql.sql
+++ sql/Mysql.sql
@@ -1,26 +1,45 @@
-create table ipl.deliveries(
-match_id varchar(50),
-inning varchar(50),
-batting_team varchar(50),
-bowling_team varchar(50),
-over varchar(50),
-ball varchar(50),
-batsman varchar(50),
-non_striker varchar(50),
-bowler varchar(50),
-is_super_over varchar(50),
-wide_runs varchar(50),
-bye_runs varchar(50),
-legbye_runs varchar(50),
-noball_runs varchar(50),
-penalty_runs varchar(50),
-batsman_runs varchar(50),
-extra_runs varchar(50),
-total_runs varchar(50),
-player_dismissed varchar(50),
-dismissal_kind varchar(50),
-fielder varchar(50)
+CREATE TABLE `sdf_schema`.`sdf_framework_source_one` (
+  `id` int DEFAULT NULL,
+  `first_name` text,
+  `last_name` text,
+  `accountnumber` text,
+  `cardnumber` bigint DEFAULT NULL
 );
+
+CREATE TABLE `sdf_schema`.`sdf_framework_source_two` (
+  `id` int DEFAULT NULL,
+  `first_name` text,
+  `last_name` text,
+  `accountnumber` text,
+  `cardnumber` bigint DEFAULT NULL
+);
+
+CREATE TABLE `sdf_schema`.`spark_sdf_join`(
+id text,
+x_first_name text,
+x_last_name text,
+x_cardnumber text,
+x_accountnumber text,
+y_first_name text,
+y_last_name text,
+y_cardnumber text,
+y_accountnumber text
+);
+
+select
+x.id as id,
+x.first_name as x_first_name,
+x.last_name as x_last_name,
+x.cardnumber as x_cardnumber,
+x.accountnumber as x_accountnumber,
+y.first_name as y_first_name,
+y.last_name as y_last_name,
+y.cardnumber as y_cardnumber,
+y.accountnumber as y_accountnumber
+from sdf_framework_source_one x
+left join sdf_framework_source_two y
+on x.id=y.id;
+
 
 create table sparkdataflow.t1 as Select m.batsman,b.city
 from mysqlread m
@@ -28,4 +47,5 @@ join books b
 on
 m.match_id=b.id
 where b.city="Hyderabad";
+
 select count(*) from sparkdataflow.t1;
