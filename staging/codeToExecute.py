bfdfe9fcfb955e4ae1a5e7ee4bd8e9c01a7a0239
abhishek009 <abhisheksingh2009iu@gmail.com>
2024-06-12 00:23:45 +0800

    Code changes for databricks

--- staging/codeToExecute.py
+++ staging/codeToExecute.py
@@ -1,16 +1,1 @@
 
-import os
-import pyspark.sql import SparkSession,DataFrame
-
-if __name__ == '__main__':
-	sparkSession = SparkSession.builder().getOrCreate()
-	df = spark.read.table(f""YOUR_SCHEMA_NAME.etl_framework_source_one"")
-	df.createOrReplaceTempView(etl_framework_source_one)
-	df_transform = spark.sql("Select * from etl_framework_source_one")
-	df_transform.createOrReplaceTempView(f"t1")
-
-df.write
-        .format("csv")
-        .options("Map(delimiter -> \t)")
-        .mode(append)
-        .save(/your/hdfs/location)
